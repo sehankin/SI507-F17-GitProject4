961db1c4fe8cae425019d618721eaf37b268f260
Jacob Haspiel <haspiel@umich.edu>
2017-10-25 11:51:28 -0400

    updating cache method

--- sample_oauth1_code.py
+++ sample_oauth1_code.py
@@ -1,5 +1,5 @@
-# OAuth1 Code to access data from the Twitter API...
-import requests_oauthlib
+# OAuth1 Code to access data from the Twitter API... Adding additional functionality for caching
+import requests_oauthlib #pulls in request library
 import webbrowser
 import json
 
@@ -214,6 +214,41 @@ print(type(ids))
 
 # a super simple version of "caching"
 # save the data we got back and collected in a file to check it out
+
+# adding additional code on caching... need to adapt...
+
+# def params_unique_combination(baseurl, params_d, private_keys=["api_key"]):
+#     alphabetized_keys = sorted(params_d.keys())
+#     res = []
+#     for k in alphabetized_keys:
+#         if k not in private_keys:
+#             res.append("{}-{}".format(k, params_d[k]))
+#     return baseurl + "_".join(res)
+
+# def sample_get_cache_ouath_data(search_term,media_term="all"):
+#     CACHE_FNAME = 'cache_file_name.json'
+#     try:
+#         cache_file = open(CACHE_FNAME, 'r')
+#         cache_contents = cache_file.read()
+#         CACHE_DICTION = json.loads(cache_contents)
+#         cache_file.close()
+#     except:
+#         CACHE_DICTION = {}
+#     baseurl = "https://itunes.apple.com/search"
+#     params = {}
+#     params["media"] = media_term
+#     params["term"] = search_term
+#     unique_ident = params_unique_combination(baseurl, params)
+#     if unique_ident in CACHE_DICTION:
+#         return CACHE_DICTION[unique_ident]
+#     else:
+#         CACHE_DICTION[unique_ident] = json.loads(requests.get(baseurl, params=params).text)
+#         full_text = json.dumps(CACHE_DICTION)
+#         cache_file_ref = open(CACHE_FNAME,"w")
+#         cache_file_ref.write(full_text)
+#         cache_file_ref.close()
+#         return CACHE_DICTION[unique_ident]
+
 fr = open("paging_nested.txt","w")
 fr.write(json.dumps(collected_tweets))
 fr.close()
